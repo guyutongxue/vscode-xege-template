c2d7fac46d31ca188196844b6010f19e81d4012a
Guyutongxue <guyutongxue@163.com>
2022-11-14 11:58:56 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,21 +3,19 @@
 -- 添加远程 xege 依赖。不可以修改。
 add_requires("xege")
 
--- 解决msvc编译器下的bug
-if is_plat("windows") then
-    add_cxflags("/utf-8")
-end
-
-set_languages("c++17")
-
 -- 项目名称。可以修改为任意名字。
 target("my-ege-project")
-
-    -- 项目生成二进制可执行文件（而非库）。不可以修改。
-    set_kind("binary")
-
+    
     -- 添加 xege 库依赖。不可以修改。
     add_packages("xege")
     
     -- 设置要编译的源文件。
     add_files("src/*.cpp")
+    
+    -- 令 MSVC 使用 UTF-8 编码
+    if is_plat("windows") then
+        add_cxflags("/utf-8")
+    end
+    
+    -- 设置以 C++17 标准编译
+    set_languages("c++17")
